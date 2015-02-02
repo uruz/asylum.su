9f8464614bebe0bb557d58ccc054566409199bda
Alexey Boriskin <boriskin@adwz.ru>
2015-02-03 00:15:27 +0300

    view for tests

--- site_asylum/urls.py
+++ site_asylum/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.http.response import HttpResponse
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'site_asylum.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', lambda req: HttpResponse(b'Asylum.su')),
     url(r'^delirium/', include('site_asylum.apps.delirium.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
